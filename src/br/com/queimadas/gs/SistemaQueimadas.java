60dcc7b7e88ce8d4884ec1371bc87b22241639d2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-06-04 10:35:29 -0300

    Novas funcionalidades: Atualizar Status e Consultar Status

--- src/br/com/queimadas/gs/SistemaQueimadas.java
+++ src/br/com/queimadas/gs/SistemaQueimadas.java
@@ -8,11 +8,17 @@ import java.util.Scanner;
 public class SistemaQueimadas {
     private Scanner scanner = new Scanner(System.in);
     private List<Queimada> listaQueimadas;
+    private List<Queimada> listaAtivas;
+    private List<Queimada> listaControladas;
+    private List<Queimada> listaResolvidas;
     private int proximoID;
     int opcao = -1;
 
     public SistemaQueimadas() {
         this.listaQueimadas = new ArrayList<>();
+        this.listaAtivas = new ArrayList<>();
+        this.listaControladas = new ArrayList<>();
+        this.listaResolvidas = new ArrayList<>();
         this.proximoID = 1;
     }
 
@@ -37,10 +43,13 @@ public class SistemaQueimadas {
 
         Queimada queimada = new Queimada(id, localizacao, horario);
         listaQueimadas.add(queimada);
+        listaAtivas.add(queimada);
 
         System.out.println("Você deseja ADICIONAR Causa e Tamanho da área impactada?\n" +
-                "1 - Sim\n" +
-                "2 - Não");
+                           "Ou somente o Tamanho da Área?\n" +
+                           "1 - Causa e tamanho da área\n" +
+                           "2 - Tamanho da área\n" +
+                           "3 - Não desejo informar");
         opcao = scanner.nextInt();
         scanner.nextLine();
 
@@ -53,6 +62,12 @@ public class SistemaQueimadas {
 
             queimada.registrarDetalhes(area,causaQueimada);
             System.out.println("Detalhes registrados com sucesso, informações da queimada: \n " + queimada);
+        }else if(opcao == 2){
+            System.out.println("Qual o tamanho da área afetada em hectáres? (Caso for especificar, usar vírgula) ");
+            double area = scanner.nextDouble();
+            scanner.nextLine();
+            queimada.registrarDetalhes(area);
+            System.out.println("Tamanho da área atualizada! Informações da queimada: " + queimada);
         }else{
             System.out.println("Queimada registrada!");
         }
@@ -93,6 +108,14 @@ public class SistemaQueimadas {
                     queimadaEncontrada.mudarStatus(statusQueimada);
                     System.out.println("Status da Queimada ID" + queimadaEncontrada.getId() + " alterado para: " + statusQueimada);
                     System.out.println(queimadaEncontrada);
+
+                    if(statusQueimada.equalsIgnoreCase("Controlado")){
+                        listaControladas.add(queimadaEncontrada);
+                        listaAtivas.remove(queimadaEncontrada);
+                    }else{
+                        listaResolvidas.add(queimadaEncontrada);
+                        listaAtivas.remove(queimadaEncontrada);
+                    }
                 }
             }else{
                 System.out.println("ID não encontrado");
@@ -100,14 +123,30 @@ public class SistemaQueimadas {
         }
     }
 
+    public void verificarStatus(){
+        System.out.println("Queimadas Ativas: \n");
+        System.out.println("--------------------------\n");
+        System.out.println(listaAtivas + "\n");
+        System.out.println("--------------------------\n");
+        System.out.println("Queimadas Controladas: \n");
+        System.out.println("--------------------------\n");
+        System.out.println(listaControladas + "\n");
+        System.out.println("--------------------------\n");
+        System.out.println("Queimadas Resolvidas: \n");
+        System.out.println("--------------------------\n");
+        System.out.println(listaResolvidas);
+        System.out.println("--------------------------\n");
+    }
+
     public void iniciar() {
         while(opcao != 0){
             String mensagemSistema = """
                     BEM VINDO AO SISTEMA DE QUEIMADAS, O QUE DESEJA FAZER?
                     -----------------------------------------------------
-                        1- Cadastrar Queimadas
+                        1- Cadastrar Queimada
                         2- Listar Queimadas
                         3- Atualizar Queimada
+                        4- Status de Queimadas
                         0- Sair
                     -----------------------------------------------------
                     """;
@@ -116,19 +155,21 @@ public class SistemaQueimadas {
             opcao = scanner.nextInt();
             scanner.nextLine();
 
-            if (opcao == 1 || opcao == 2 || opcao == 3 ||  opcao == 0){
+            if (opcao == 1 || opcao == 2 || opcao == 3 || opcao == 4 || opcao == 0){
                 if(opcao == 1){
                     cadastroQueimadas();
                 }else if(opcao == 2){
                     listarQueimadas();
                 }else if(opcao == 3){
                     atualizarQueimada();
+                }else if(opcao == 4){
+                    verificarStatus();
                 }else{
-                    System.out.println("Adeus!");
+                    System.out.println("Saindo do sistema, Até logo!");
                     break;
                 }
             }else {
-                System.out.println("Opção inválida! Por favor, digite 0, 1 ou 2.");
+                System.out.println("Opção inválida! Por favor, digite 0, 1, 2, 3 ou 4");
                 opcao = -1;
             }
         }
